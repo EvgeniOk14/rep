d7c32d239fbc53a292fb42e951ad0c84a42b6860
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-05 20:58:02 +0300

    added new project: homeTusk1

---
+++ homeTusk1/Program.cs
@@ -0,0 +1,7 @@
+﻿int n = new Random().Next(100,1000);
+Console.WriteLine("Random number n = "+ n);
+int x1 = n / 100;
+int x3 = n % 10;
+int y = x1*10 + x3;
+Console.WriteLine("the new number is consisted of the fist unit of random number and the third unit of one is: " + y);
+
