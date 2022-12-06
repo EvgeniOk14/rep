274542ccded2d2ab83b8f77752794d3b02802a0c
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-06 14:10:56 +0300

    added a new project: homeTusk4

---
+++ homeTusk4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+int i = 0;
+int n = Convert.ToInt32(Console.ReadLine());
+  int max1 = n;
+  int max2 = n;
+  while (n != 0)
+ {
+      n  = Convert.ToInt32(Console.ReadLine());  
+             if (n > max1)
+            {   
+             max2 = max1;
+             max1 = n;
+            }
+            else if (n> max2 && n < max1)
+            {
+              max2 = n;
+            }
+       i = i + 1;
+ }
+    Console.WriteLine("end");
+    Console.WriteLine("количество вводов равно: " + i);
+    Console.WriteLine("первый максимум равен: " + max1);      
+    Console.WriteLine("ВТОРОЙ МАКСИМУМ РАВЕН: " + max2); 
